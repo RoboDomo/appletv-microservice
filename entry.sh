0084b83f2a7522d4769a525a305d9aff450133ff
Michael Schwartz <mykesx@gmail.com>
2019-05-13 08:49:18 -0700

    works on host, not in container

--- entry.sh
+++ entry.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 service dbus restart
 service avahi-daemon restart
+ps -aux | grep avahi
+ps -aux | grep dbus
 npm start
--- shell.sh
+++ shell.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 docker run \
-  --net=host \
   -it  \
+  -v $PWD:/home/app \
+  -v /home/app/node_modules \
   --rm \
   --name appletv-microservice \
-  -v /var/run/dbus/system_bus_docket:/var/run/dbus/system_bus_socket \
   robodomo/appletv-microservice \
   /bin/bash
